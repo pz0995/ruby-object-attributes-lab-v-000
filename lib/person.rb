ee11546bc90af07080a3c1523aee73263be631fb
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-15 06:58:56 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def initialize(name)
+    @name = name
+    @job
+  end
+ 
+  def name
+    @name
+  end
+end
+
+john smith = Person.new("John Smith")
+john smith.name
+johnsmith.job = "mailman"
