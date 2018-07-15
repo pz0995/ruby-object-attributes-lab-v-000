2354416659343b4f7239ff52b7b46013d5bec8fb
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-15 08:22:41 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,18 @@
 class Person
-
-  def initialize(name)
-    @name = name
-  end
-
   def name
     @name
   end
 
-  def name=(new_name)
-      @name = new_name
+  def name=(name)
+    @name = name
   end
 
   def job
-      @name
+    @job
+  end
+
+  def job=(job)
+    @job = job
   end
 end
 
